30b1f644b14dfcb61e4e8b2b3cf272c0fa448763
frc195programming <frc195programming@gmail.com>
2023-02-05 02:24:20 +0000

    added virtual intake

--- src/intake_node/main.py
+++ src/intake_node/main.py
@@ -1,6 +1,9 @@
 #!/usr/bin/env python3
 
 import rospy
+
+from ck_utilities_py_node.transform_links import *
+from ck_utilities_py_node.rviz_shapes import *
 from threading import Thread
 
 
@@ -10,6 +13,71 @@ from frc_robot_utilities_py_node.frc_robot_utilities_py import *
 from frc_robot_utilities_py_node.RobotStatusHelperPy import RobotStatusHelperPy, Alliance, RobotMode, BufferedROSMsgHandlerPy
 from ck_ros_msgs_node.msg import Intake_Control, Intake_Status
 
+
+def publish_intake_1_link(roll_degrees : float, yaw_degrees: float, Pinched: bool):
+    transform = Transform()
+   # transform.linear.z = 0 # 
+    if not Pinched:
+        transform.angular.roll = math.radians(roll_degrees)
+    else:
+        transform.angular.roll = math.radians(15)
+
+    transform.angular.yaw = math.radians(yaw_degrees)
+    transform.linear.z = 0.4064
+
+
+    transform_link = TransformLink("intake_1", "arm_extender")
+    transform_link.set_transform(transform)
+    transform_link.publish()
+
+def publish_intake_2_link(roll_degrees : float, yaw_degrees: float, Pinched: bool):
+    transform = Transform()
+    # transform.linear.z = 0 # 
+    if not Pinched:
+        transform.angular.roll = math.radians(roll_degrees)
+    else:
+        transform.angular.roll = math.radians(-15)
+
+    transform.angular.yaw = math.radians(yaw_degrees)
+    transform.linear.z = 0.4064
+
+    transform_link = TransformLink("intake_2", "arm_extender")
+    transform_link.set_transform(transform)
+    transform_link.publish()
+
+def publish_arrow_1_link(roll_degrees : float, intake: float):
+    transform = Transform()
+    # transform.linear.z = 0 # 
+
+    if intake > 0:
+        transform.angular.roll = math.radians(roll_degrees)
+    elif intake < 0:
+        transform.angular.roll = math.radians(180-roll_degrees)
+    else:
+        transform.angular.roll = math.radians(90)
+
+    transform.linear.z = 1.5
+
+    transform_link = TransformLink("arrow_1", "arm_extender")
+    transform_link.set_transform(transform)
+    transform_link.publish()
+    
+def publish_arrow_2_link(roll_degrees : float, intake: float):
+    transform = Transform()
+    # transform.linear.z = 0 # 
+
+    if intake > 0 :
+        transform.angular.roll = math.radians(roll_degrees)
+    elif intake < 0:
+        transform.angular.roll = math.radians(180-roll_degrees)
+    else:
+        transform.angular.roll = math.radians(-90)
+
+    transform.linear.z = 1.5
+
+    transform_link = TransformLink("arrow_2", "arm_extender")
+    transform_link.set_transform(transform)
+    transform_link.publish()
 
 def ros_func():
     global hmi_updates
@@ -49,6 +117,11 @@ def ros_func():
                     pincherSolenoid.set(SolenoidState.ON)
                 else:
                     pincherSolenoid.set(SolenoidState.OFF)
+        
+        publish_intake_1_link(45, 25, pincherSolenoid.get() == SolenoidState.ON)
+        publish_intake_2_link(-45, 25, pincherSolenoid.get() == SolenoidState.ON)
+        publish_arrow_1_link(45, 190)#intakeRollerMotor.get_sensor_velocity())
+        publish_arrow_2_link(-45, 190)#intakeRollerMotor.get_sensor_velocity())
 
         status_message = Intake_Status()
         status_publisher.publish(status_message)
@@ -60,6 +133,38 @@ def ros_main(node_name):
     rospy.init_node(node_name)
     register_for_robot_updates()
 
+    intake_rectangle_1 = Cube("robot_parts", 4, "intake_1")
+    intake_rectangle_1_transform = Transform()
+    intake_rectangle_1_transform.linear.z = 0.2032 #12 in
+    intake_rectangle_1.set_transform(intake_rectangle_1_transform)
+    intake_rectangle_1.set_scale(Scale(0.0508, 0.0508, 0.4064)) #14 out (11.995, 16, 2) 
+    intake_rectangle_1.set_color(Color(.7, .7, .7, 1.0))
+    intake_rectangle_1.publish()
+
+    intake_rectangle_2 = Cube("robot_parts", 5, "intake_2")
+    intake_rectangle_2_transform = Transform()
+    intake_rectangle_2_transform.linear.z = 0.2032 #12 in
+    intake_rectangle_2.set_transform(intake_rectangle_2_transform)
+    intake_rectangle_2.set_scale(Scale(0.0508, 0.0508, 0.4064)) #14 out (11.995, 16, 2) 
+    intake_rectangle_2.set_color(Color(.7, .7, .7, 1.0))
+    intake_rectangle_2.publish()
+
+    arrow_rectangle_1 = Cube("arrow", 0, "arrow_1")
+    arrow_rectangle_1_transform = Transform()
+    arrow_rectangle_1_transform.linear.z = 0.2032 #12 in
+    arrow_rectangle_1.set_transform(arrow_rectangle_1_transform)
+    arrow_rectangle_1.set_scale(Scale(0.0508, 0.0508, 0.4064)) #14 out (11.995, 16, 2) 
+    arrow_rectangle_1.set_color(Color(.7, .7, .7, 1.0))
+    arrow_rectangle_1.publish()
+
+    arrow_rectangle_2 = Cube("arrow", 1, "arrow_2")
+    arrow_rectangle_2_transform = Transform()
+    arrow_rectangle_2_transform.linear.z = 0.2032 #12 in
+    arrow_rectangle_2.set_transform(arrow_rectangle_2_transform)
+    arrow_rectangle_2.set_scale(Scale(0.0508, 0.0508, 0.4064)) #14 out (11.995, 16, 2) 
+    arrow_rectangle_2.set_color(Color(.7, .7, .7, 1.0))
+    arrow_rectangle_2.publish()
+ 
     t1 = Thread(target=ros_func)
     t1.start()
 
