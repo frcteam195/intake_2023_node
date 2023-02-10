b9417e674b2e6a7d34e58169c3a8e0961a1a34f3
Robert Hilton <robert.a.hilton.jr@gmail.com>
2023-02-10 04:39:24 +0000

    update shapes

--- src/intake_node/intake_simulation.py
+++ src/intake_node/intake_simulation.py
@@ -4,7 +4,7 @@ import math
 
 class IntakeSimulation:
     def __init__(self) -> None:
-        intake_rectangle_1 = Cube("robot_parts", 4, "intake_1")
+        intake_rectangle_1 = Cube("intake_1")
         intake_rectangle_1_transform = Transform()
         intake_rectangle_1_transform.linear.z = 0.2032 #12 in
         intake_rectangle_1.set_transform(intake_rectangle_1_transform)
@@ -12,7 +12,7 @@ class IntakeSimulation:
         intake_rectangle_1.set_color(Color(.7, .7, .7, 1.0))
         intake_rectangle_1.publish()
 
-        intake_rectangle_2 = Cube("robot_parts", 5, "intake_2")
+        intake_rectangle_2 = Cube("intake_2")
         intake_rectangle_2_transform = Transform()
         intake_rectangle_2_transform.linear.z = 0.2032 #12 in
         intake_rectangle_2.set_transform(intake_rectangle_2_transform)
@@ -20,7 +20,7 @@ class IntakeSimulation:
         intake_rectangle_2.set_color(Color(.7, .7, .7, 1.0))
         intake_rectangle_2.publish()
 
-        arrow = Arrow("arrow", 2, "arrow")
+        arrow = Arrow("arrow")
         arrow_transform = Transform()
         arrow.set_transform(arrow_transform)
         arrow.set_scale(Scale(0.5, 0.1, 0.1)) #14 out (11.995, 16, 2)
